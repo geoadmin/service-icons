7f1580f1e9c7862b7386038d3e97a8824d750bf6
Brice Schaffner <brice.schaffner@swisstopo.ch>
2020-07-27 12:49:59 +0200

    Change the code max line length from 120 to 100
    Also corrected the code accordingly

--- app/middleware.py
+++ app/middleware.py
@@ -1,7 +1,8 @@
 class ReverseProxy(object):
     """
-    Reverse proxies can cause some problems within applications, as they change routes, redirect traffic, and
-    applications might have errors because of that. This piece of middlewar make sure everything runs smoothly.
+    Reverse proxies can cause some problems within applications, as they change routes, redirect
+    traffic, and applications might have errors because of that. This piece of middlewar make sure
+    everything runs smoothly.
     """
 
     def __init__(self, app, script_name=None, scheme=None, server=None, port=None):
@@ -13,13 +14,15 @@ class ReverseProxy(object):
 
     def __call__(self, environ, start_response):
         """
-        This function modifies the environment received by the WSGI, mostly making sure we serve the right routes and
-        that the application answers as if it were the initial host (for example, an error should say that the site at
-        reverse-proxied.admin.ch/generic_name/generate encountered a problem, not that it was at
-        internal.server/generate
+        This function modifies the environment received by the WSGI, mostly making sure we serve
+        the right routes and that the application answers as if it were the initial host (for
+        example, an error should say that the site at reverse-proxied.admin.ch/generic_name/generate
+        encountered a problem, not that it was at internal.server/generate
+
         :param environ: the WSGI environment
-        :param start_response: A callable accepting headers, a status code and can accept an exception to start the
-        response
+        :param start_response: A callable accepting headers, a status code and can accept an
+        exception to start the response
+
         :return: a call to the wsgi app, with an updated WSGI environment.
         """
         """
@@ -28,11 +31,11 @@ class ReverseProxy(object):
         HTTP_X_SCRIPT_NAME will be /generic_name, and PATH_INFO would be /generic_name/generate.
 
         PATH_INFO is used to handle the route called (in this case, it would use /generate)
-        SCRIPT_NAME is used to prefix the routes so that the application returns a correct answer (namely: that the
-        query was hitting /generic_name/generate).
+        SCRIPT_NAME is used to prefix the routes so that the application returns a correct answer
+        (namely: that the query was hitting /generic_name/generate).
         """
-        # The syntax here means : try to get HTTP_X_SCRIPT_NAME, or get me an empty string, and the command or, in the
-        # context of a string, returns the first "truthy" value
+        # The syntax here means : try to get HTTP_X_SCRIPT_NAME, or get me an empty string, and the
+        # command or, in the context of a string, returns the first "truthy" value
         script_name = environ.get('HTTP_X_SCRIPT_NAME', '') or self.script_name
         if script_name:
             environ['SCRIPT_NAME'] = script_name
--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,10 @@ class StandaloneApplication(BaseApplication):  # pylint: disable=abstract-method
         super(StandaloneApplication, self).__init__()
 
     def load_config(self):
-        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        config = {
+            key: value for key,
+            value in self.options.items() if key in self.cfg.settings and value is not None
+        }
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
