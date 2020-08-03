717e34d2f66325377de2bc67acebb2927eeac12b
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2020-08-03 12:02:40 +0200

    added CORS (similar to service qrcode) / error msgs are now jsonified

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,46 @@
-from flask import Flask
+import re
+
+from flask import Flask, request, abort, json
+from werkzeug.exceptions import HTTPException
+
+from app.helpers import make_error_msg
+from app.helpers.url import ALLOWED_DOMAINS_PATTERN
 from app.middleware import ReverseProxy
 
 # Standard Flask application initialisation
 
 app = Flask(__name__)
 app.wsgi_app = ReverseProxy(app.wsgi_app, script_name='/')
+
+
+# Add CORS Headers to all request
+@app.after_request
+def add_cors_header(response):
+    if (
+        'Origin' in request.headers and
+        re.match(ALLOWED_DOMAINS_PATTERN, request.headers['Origin'])
+    ):
+        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+        response.headers['Access-Control-Allow-Methods'] = 'GET'
+    return response
+
+
+# Reject request from non allowed origins
+@app.before_request
+def validate_origin():
+    if (
+        'Origin' in request.headers and
+        not re.match(ALLOWED_DOMAINS_PATTERN, request.headers['Origin'])
+    ):
+        abort(make_error_msg(403, 'Not allowed'))
+
+
+# Register error handler to make sure that every error returns a json answer
+@app.errorhandler(HTTPException)
+def handle_exception(err):
+    """Return JSON instead of HTML for HTTP errors."""
+    return make_error_msg(err.code, err.description)
+
 
 from app import routes  # pylint: disable=wrong-import-position
 
