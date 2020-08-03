0a9fd82eacbf5c395ed435f9c56fe544ea88f55f
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2020-08-03 12:09:16 +0200

    removed patch check / jsonified error msg / moved check for integer values to route template

--- app/helpers/check_functions.py
+++ app/helpers/check_functions.py
@@ -13,9 +13,7 @@ def check_color_channels(r, g, b):
     :param b: color channel value of the blue channel.
     :return:  verified r, g and b values.
     """
-    if not (r.isdigit() and g.isdigit() and b.isdigit()):
-        abort(make_error_msg(400, "Color channel values must be integers."))
-    elif not ((0 <= int(r) <= 255) and (0 <= int(g) <= 255) and (0 <= int(b) <= 255)):
+    if not ((0 <= r <= 255) and (0 <= g <= 255) and (0 <= b <= 255)):
         abort(
             make_error_msg(400, "Color channel values must be integers in the range of 0 to 255.")
         )
