b097ce418af0e05e13e601a13f43df919e65789f
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2020-08-03 12:07:54 +0200

    added CORS (similar to service qrcode) / removed Python 2 support / added route prefix / added type checking to route template

---
+++ app/helpers/url.py
@@ -0,0 +1,10 @@
+import re
+
+# Definition of the allowed domains for CORS implementation in ../app/__init.py__
+ALLOWED_DOMAINS = [
+    r'.*\.geo\.admin\.ch',
+    r'.*bgdi\.ch',
+    r'.*\.swisstopo\.cloud',
+]
+
+ALLOWED_DOMAINS_PATTERN = '({})'.format('|'.join(ALLOWED_DOMAINS))
--- app/routes.py
+++ app/routes.py
@@ -1,66 +1,48 @@
+from io import BytesIO
 import os.path
-# using werkzeug.exceptions in order to get rid of pyramid
-# from pyramid.httpexceptions import HTTPBadRequest and other flask stuff
-# from pyramid.view import view_config
-# from pyramid.response import Response
-from flask import Response
-import six  # for compatibility Python 2 / 3
+from flask import Response, make_response, jsonify, abort
 from PIL import Image
 from app import app
-from app.helpers import check_color_channels
-from app.helpers import check_path
+from app.helpers.check_functions import check_color_channels
+from app.helpers.route import prefix_route
+from app.helpers import make_error_msg
 
-# Python2/3 compatibility
-if six.PY3:
-    from io import BytesIO
-else:
-    from StringIO import StringIO
+# add route prefix
+app.route = prefix_route(app.route, '/v4/color/')
 
 
 @app.route('/checker')
-@app.route('/checker/')
 def checker_page():
     """
     Just a route for the health check
     :return: OK with a 200 status code
     """
-    return "OK", 200
+    return make_response(jsonify({'success': True, 'message': 'OK'}))
 
 
-@app.route('/color/<r>,<g>,<b>/<filename>/', methods=['GET'])
-@app.route('/color/<r>,<g>,<b>/<filename>', methods=['GET'])
+@app.route('/<int:r>,<int:g>,<int:b>/<string:filename>', methods=['GET'])
 def color(r, g, b, filename):
     """
     This endpoint verifies the parameters of the request.
     BadRequests errors will be raised in case of not properly defined parameters.
-    If all parameters are well defined the colored image (symbol) is returned.
+    If all parameters are well defined, the colored image (symbol) is returned.
     :param r: integer value in the range between 0 and 255 for the red channel
     :param g: integer value in the range between 0 and 255 for the green channel
     :param b: integer value in the range between 0 and 255 for the blue channel
     :param filename: name of the file containing the image/symbol to be colored
     :return:
     """
-    check_color_channels(r, g, b)
-    r = int(r)
-    g = int(g)
-    b = int(b)
+    r, g, b = check_color_channels(r, g, b)
 
-    # the images are currently stored in a subfolder calles "maki".
-    # is it necessary to keep this subfolder, or is ../static/images/ ok?
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../static/images/', filename))
+    if not os.path.isfile(path):
+        abort(make_error_msg(400, "The image to colorize doesn\'t exist"))
 
-    check_path(path)
-
-    mask = Image.open(path)
-
-    if mask.mode == 'P':
-        mask = mask.convert('RGBA')
-    img = Image.composite(Image.new("RGB", mask.size, (r, g, b)), mask, mask)
-    if six.PY3:
+    with Image.open(path) as mask:
+        if mask.mode == 'P':
+            mask = mask.convert('RGBA')
+        img = Image.composite(Image.new("RGB", mask.size, (r, g, b)), mask, mask)
         output = BytesIO()
-    else:
-        output = StringIO()
-    img.save(output, format='PNG')
+        img.save(output, format='PNG')
 
     return Response(output.getvalue(), mimetype='image/png')
-    
