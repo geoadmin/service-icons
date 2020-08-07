3d9950aaa3e227a7273f5db3cb213d245d2a388b
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2020-08-07 11:11:01 +0200

    added simple version handling

--- app/routes.py
+++ app/routes.py
@@ -12,30 +12,38 @@ from app.helpers.route import prefix_route
 from app.helpers import make_error_msg
 
 # add route prefix
-app.route = prefix_route(app.route, '/v4/color/')
+app.route = prefix_route(app.route, '/v<int:ver>/color')
 
 
 @app.route('/checker')
-def checker_page():
+def checker_page(ver):
     """
-    Just a route for the health check
-    :return: OK with a 200 status code
+    Just a route for the health check.
+    :param v: integer value of the service's version (inherited from prefix_route).
+    :return: OK with a 200 status code or raise error in case of unsupported version.
     """
+    if ver > 4:
+        abort(make_error_msg(400, "unsupported version of sercive"))
+
     return make_response(jsonify({'success': True, 'message': 'OK'}))
 
 
 @app.route('/<int:r>,<int:g>,<int:b>/<string:filename>', methods=['GET'])
-def color(r, g, b, filename):  # pylint: disable=invalid-name
+def color(ver, r, g, b, filename):  # pylint: disable=invalid-name
     """
     This endpoint verifies the parameters of the request.
     BadRequests errors will be raised in case of not properly defined parameters.
     If all parameters are well defined, the colored image (symbol) is returned.
-    :param r: integer value in the range between 0 and 255 for the red channel
-    :param g: integer value in the range between 0 and 255 for the green channel
-    :param b: integer value in the range between 0 and 255 for the blue channel
-    :param filename: name of the file containing the image/symbol to be colored
+    :param v: integer value of the service's version (inherited from prefix_route).
+    :param r: integer value in the range between 0 and 255 for the red channel.
+    :param g: integer value in the range between 0 and 255 for the green channel.
+    :param b: integer value in the range between 0 and 255 for the blue channel.
+    :param filename: name of the file containing the image/symbol to be colored.
     :return:
     """
+    if ver > 4:
+        abort(make_error_msg(400, "unsupported version of service"))
+
     r, g, b = check_color_channels(r, g, b)
 
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../static/images/', filename))
