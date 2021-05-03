21112dbf79ed99a57058d95b2ae5ab7ec276f166
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2021-05-03 06:05:39 +0200

    moved helpers in app.helpers.utils.py | used with statement for subprocess.Popen()

--- service_launcher.py
+++ service_launcher.py
@@ -1,7 +1,7 @@
 '''Service launcher to use Flask without wsgi.py
 '''
 from app import app  # pylint: disable=unused-import
-from app.helpers import init_logging
+from app.helpers.utils import init_logging
 
 # Initialize Logging using JSON format for all loggers and using the Stream Handler.
 init_logging()
