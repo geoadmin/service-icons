9562e6382752456f11db5cb213c3778ccccf6e2e
Lukas Joss <lukas.joss@swisstopo.ch>
2024-01-18 10:13:22 +0100

    PB-227: Change script to read excel file instead

--- scripts/generate_babs_dic.py
+++ scripts/generate_babs_dic.py
@@ -1,42 +1,24 @@
 
 import json
 import os
-
-dir = os.listdir('../static/images/babs2')
-
-lan_dic = {
-    'F' : "fr",
-    'I' : "it",
-    "D" : "de"
-}
+import pandas as pd
 
 id_range = slice(0,3)
-lang_range = slice(4,5)
 name_range = slice(6,None)
-max_id = 117
 
-files = []
-for file in dir:
-    files.append((file[id_range],
-                  lan_dic[file[lang_range]],
-                  file[name_range].split('.png')[0])
-                )
-
-#sort names by id and language
-files.sort(key=lambda x: x[1])
-files.sort(key=lambda x: x[0])
+# read by default 1st sheet of an excel file
+df = pd.read_excel('../../tmp/Namenliste.xlsx')
+print(df)
 
 json_dic = {}
-for id in range(0,max_id):
-    string_id = str(id).zfill(3)
-    icon_dic={}
-    list = []
-    for f in files:
-        if(f[0]==string_id):
-            list.append(f)
-            icon_dic[f[1]]=(f[2])
-    if(list):
-        json_dic["ICON_"+string_id]=icon_dic
+for index, row in df.iterrows():
+    string_id=row['D'][id_range]
+    icon_dic={
+                "de": row['D'][name_range].split('.svg')[0],
+                "fr": row['F'][name_range].split('.svg')[0],
+                "it": row['I'][name_range].split('.svg')[0]
+              }
+    json_dic["ICON_"+string_id]=icon_dic
 
-with open("../static/images/babs_2_dictionary.json", "w") as outfile:
+with open("../json/babs_2_dictionary.json", "w") as outfile:
     json.dump(json_dic, outfile, indent=4)
